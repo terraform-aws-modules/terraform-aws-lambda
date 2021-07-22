41ffd37e825701f133656112d10817e1b224ddc1
Matej Liner <mliner@ohpen.com>
2021-07-22 12:14:27 +0200

    fix: another fix

--- package.py
+++ package.py
@@ -893,7 +893,7 @@ def install_pip_requirements(query, requirements_file):
                                              chown_mask, '.'])]
                 shell_command = [' '.join(shell_command)]
                 check_call(docker_run_command(
-                    temp_dir, shell_command, runtime,
+                    '.', shell_command, runtime,
                     image=docker_image_tag_id,
                     shell=True, ssh_agent=with_ssh_agent,
                     pip_cache_dir=pip_cache_dir,
