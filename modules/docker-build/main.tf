17e40008f546ecc87a99871e36e25385f76574a4
Anton Babenko <anton@antonbabenko.com>
2021-11-05 21:20:07 +0100

    Update modules/docker-build/main.tf

--- modules/docker-build/main.tf
+++ modules/docker-build/main.tf
@@ -5,7 +5,7 @@ data "aws_caller_identity" "this" {}
 data "aws_ecr_authorization_token" "token" {}
 
 locals {
-  ecr_address    = var.ecr_address != null ? var.ecr_address : format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.current.name)
+  ecr_address    = coalesce(var.ecr_address, format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.current.name))
   ecr_repo       = var.create_ecr_repo ? aws_ecr_repository.this[0].id : var.ecr_repo
   image_tag      = coalesce(var.image_tag, formatdate("YYYYMMDDhhmmss", timestamp()))
   ecr_image_name = format("%v/%v:%v", local.ecr_address, local.ecr_repo, local.image_tag)
