83ea40f5bdc4772ee2d17758c43479350109e3e3
Thiha Soe <8920329+tsoe77@users.noreply.github.com>
2022-03-01 08:40:47 +0000

    added missing version constraint for provider "archive"

--- examples/code-signing/versions.tf
+++ examples/code-signing/versions.tf
@@ -2,6 +2,13 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws = ">= 3.17"
+    aws = { 
+      source  = "hashicorp/aws"
+      version = ">= 3.17"
+    }
+    archive_file = {
+      source = "hashicorp/archive_file"
+      version = ">= 2.2.0" 
+    }
   }
 }
