1e5eec877909c92943549e00bc8c2527c9655220
Anton Babenko <anton@antonbabenko.com>
2022-03-28 09:45:51 +0200

    feat: Updated AWS provider to version 4.8

--- examples/container-image/versions.tf
+++ examples/container-image/versions.tf
@@ -2,7 +2,13 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws    = ">= 3.19"
-    random = ">= 2"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.19"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.0"
+    }
   }
 }
