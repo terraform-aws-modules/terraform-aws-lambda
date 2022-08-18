953ccee287135da9850818b2d7411bdb72f23ae5
1Mill <1Mill@users.noreply.github.com>
2022-08-18 01:42:32 -0700

    feat!: Updated AWS provider to v4, added ECR repo force_delete argument in docker-build module (#337)

--- modules/docker-build/versions.tf
+++ modules/docker-build/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.35"
+      version = ">= 4.22"
     }
     docker = {
       source  = "kreuzwerker/docker"
