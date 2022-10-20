f9bf21df9bef0730ed3efc174fc12a79e3a5268c
Loren Gordon <lorengordon@users.noreply.github.com>
2022-10-20 07:18:56 -0700

    fix: Generates error in plan phase if runtime is not available (#358)

--- package.py
+++ package.py
@@ -656,9 +656,14 @@ class BuildPlanManager:
                 if required:
                     raise RuntimeError(
                         'File not found: {}'.format(requirements))
-            else:
-                step('pip', runtime, requirements, prefix, tmp_dir)
-                hash(requirements)
+            if not shutil.which(runtime):
+                raise RuntimeError(
+                    "Python interpreter version equal "
+                    "to defined lambda runtime ({}) should be "
+                    "available in system PATH".format(runtime))
+
+            step('pip', runtime, requirements, prefix, tmp_dir)
+            hash(requirements)
 
         def npm_requirements_step(path, prefix=None, required=False, tmp_dir=None):
             requirements = path
@@ -668,9 +673,14 @@ class BuildPlanManager:
                 if required:
                     raise RuntimeError(
                         'File not found: {}'.format(requirements))
-            else:
-                step('npm', runtime, requirements, prefix, tmp_dir)
-                hash(requirements)
+            if not shutil.which(runtime):
+                raise RuntimeError(
+                    "Nodejs interpreter version equal "
+                    "to defined lambda runtime ({}) should be "
+                    "available in system PATH".format(runtime))
+
+            step('npm', runtime, requirements, prefix, tmp_dir)
+            hash(requirements)
 
         def commands_step(path, commands):
             if not commands:
