3dac44c3e136fd41a54fc9ec8a6f817ff65a3412
Ricardo Oliveira <ricardo.o@webother.co.uk>
2023-02-13 16:09:47 +0000

    add config to replace security groups on destroy

--- variables.tf
+++ variables.tf
@@ -223,6 +223,18 @@ variable "snap_start" {
   default     = false
 }
 
+variable "replace_security_groups_on_destroy" {
+  description = "(Optional) When true, all security groups defined in vpc_security_group_ids will be replaced with the default security group after the function is destroyed. Set the replacement_security_group_ids variable to use a custom list of security groups for replacement instead."
+  type        = bool
+  default     = false
+}
+
+variable "replacement_security_group_ids" {
+  description = "(Optional) List of security group IDs to assign to orphaned Lambda function network interfaces upon destruction. replace_security_groups_on_destroy must be set to true to use this attribute."
+  type        = list(string)
+  default     = []
+}
+
 ###############
 # Function URL
 ###############
