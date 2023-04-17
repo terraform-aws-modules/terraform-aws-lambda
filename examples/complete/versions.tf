d7b3ac970b7f18be19c95ec43ce4d1fac9ae2572
Piotr Krawiec <piotr@krawiec.me>
2023-04-17 17:16:38 +0200

    feat: Add invoke_mode input (#446)

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.9"
+      version = ">= 4.63"
     }
     random = {
       source  = "hashicorp/random"
