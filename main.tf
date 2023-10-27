180da4cb0a720f7138e6504700ddfe8d9c63abfd
AJ Schmidt <ajschmidt8@users.noreply.github.com>
2023-10-27 14:00:25 -0400

    feat: Make `compatible_runtimes` optional, added sam metadata control (#493)
    Co-authored-by: Anton Babenko <anton@antonbabenko.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- main.tf
+++ main.tf
@@ -151,7 +151,7 @@ resource "aws_lambda_layer_version" "this" {
   description  = var.description
   license_info = var.license_info
 
-  compatible_runtimes      = length(var.compatible_runtimes) > 0 ? var.compatible_runtimes : [var.runtime]
+  compatible_runtimes      = length(var.compatible_runtimes) > 0 ? var.compatible_runtimes : (var.runtime == "" ? null : [var.runtime])
   compatible_architectures = var.compatible_architectures
   skip_destroy             = var.layer_skip_destroy
 
