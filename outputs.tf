c19f7ce0bd2e1b1b88df1bf690c400b9ef5a6944
Suresh Babu <sbabu@paraport.com>
2024-05-14 16:30:20 -0400

    changed function name and output module name

--- outputs.tf
+++ outputs.tf
@@ -1,162 +1,162 @@
 # Lambda Function
 output "lambda_function_arn" {
   description = "The ARN of the Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_function_arn
+  value       = module.aws_lambda_function.lambda_function_arn
 }
 
 output "lambda_function_arn_static" {
   description = "The static ARN of the Lambda Function. Use this to avoid cycle errors between resources (e.g., Step Functions)"
-  value       = module.aws_lambda_function_internal_only.lambda_function_arn_static
+  value       = module.aws_lambda_function.lambda_function_arn_static
 }
 
 output "lambda_function_invoke_arn" {
   description = "The Invoke ARN of the Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_function_invoke_arn
+  value       = module.aws_lambda_function.lambda_function_invoke_arn
 }
 
 output "lambda_function_name" {
   description = "The name of the Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_function_name
+  value       = module.aws_lambda_function.lambda_function_name
 }
 
 output "lambda_function_qualified_arn" {
   description = "The ARN identifying your Lambda Function Version"
-  value       = module.aws_lambda_function_internal_only.lambda_function_qualified_arn
+  value       = module.aws_lambda_function.lambda_function_qualified_arn
 }
 
 output "lambda_function_qualified_invoke_arn" {
   description = "The Invoke ARN identifying your Lambda Function Version"
-  value       = module.aws_lambda_function_internal_only.lambda_function_qualified_invoke_arn
+  value       = module.aws_lambda_function.lambda_function_qualified_invoke_arn
 }
 
 output "lambda_function_version" {
   description = "Latest published version of Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_function_version
+  value       = module.aws_lambda_function.lambda_function_version
 }
 
 output "lambda_function_last_modified" {
   description = "The date Lambda Function resource was last modified"
-  value       = module.aws_lambda_function_internal_only.lambda_function_last_modified
+  value       = module.aws_lambda_function.lambda_function_last_modified
 }
 
 output "lambda_function_kms_key_arn" {
   description = "The ARN for the KMS encryption key of Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_function_kms_key_arn
+  value       = module.aws_lambda_function.lambda_function_kms_key_arn
 }
 
 output "lambda_function_source_code_hash" {
   description = "Base64-encoded representation of raw SHA-256 sum of the zip file"
-  value       = module.aws_lambda_function_internal_only.lambda_function_source_code_hash
+  value       = module.aws_lambda_function.lambda_function_source_code_hash
 }
 
 output "lambda_function_source_code_size" {
   description = "The size in bytes of the function .zip file"
-  value       = module.aws_lambda_function_internal_only.lambda_function_source_code_size
+  value       = module.aws_lambda_function.lambda_function_source_code_size
 }
 
 output "lambda_function_signing_job_arn" {
   description = "ARN of the signing job"
-  value       = module.aws_lambda_function_internal_only.lambda_function_signing_job_arn
+  value       = module.aws_lambda_function.lambda_function_signing_job_arn
 }
 
 output "lambda_function_signing_profile_version_arn" {
   description = "ARN of the signing profile version"
-  value       = module.aws_lambda_function_internal_only.lambda_function_signing_profile_version_arn
+  value       = module.aws_lambda_function.lambda_function_signing_profile_version_arn
 }
 
 # Lambda Function URL
 output "lambda_function_url" {
   description = "The URL of the Lambda Function URL"
-  value       = module.aws_lambda_function_internal_only.lambda_function_url
+  value       = module.aws_lambda_function.lambda_function_url
 }
 
 output "lambda_function_url_id" {
   description = "The Lambda Function URL generated id"
-  value       = module.aws_lambda_function_internal_only.lambda_function_url_id
+  value       = module.aws_lambda_function.lambda_function_url_id
 }
 
 # Lambda Layer
 output "lambda_layer_arn" {
   description = "The ARN of the Lambda Layer with version"
-  value       = module.aws_lambda_function_internal_only.lambda_layer_arn
+  value       = module.aws_lambda_function.lambda_layer_arn
 }
 
 output "lambda_layer_layer_arn" {
   description = "The ARN of the Lambda Layer without version"
-  value       = module.aws_lambda_function_internal_only.lambda_layer_layer_arn
+  value       = module.aws_lambda_function.lambda_layer_layer_arn
 }
 
 output "lambda_layer_created_date" {
   description = "The date Lambda Layer resource was created"
-  value       = module.aws_lambda_function_internal_only.lambda_layer_created_date
+  value       = module.aws_lambda_function.lambda_layer_created_date
 }
 
 output "lambda_layer_source_code_size" {
   description = "The size in bytes of the Lambda Layer .zip file"
-  value       = module.aws_lambda_function_internal_only.lambda_layer_source_code_size
+  value       = module.aws_lambda_function.lambda_layer_source_code_size
 }
 
 output "lambda_layer_version" {
   description = "The Lambda Layer version"
-  value       = module.aws_lambda_function_internal_only.lambda_layer_version
+  value       = module.aws_lambda_function.lambda_layer_version
 }
 
 # Lambda Event Source Mapping
 output "lambda_event_source_mapping_function_arn" {
   description = "The the ARN of the Lambda function the event source mapping is sending events to"
-  value       = module.aws_lambda_function_internal_only.lambda_event_source_mapping_function_arn
+  value       = module.aws_lambda_function.lambda_event_source_mapping_function_arn
 }
 
 output "lambda_event_source_mapping_state" {
   description = "The state of the event source mapping"
-  value       = module.aws_lambda_function_internal_only.lambda_event_source_mapping_state
+  value       = module.aws_lambda_function.lambda_event_source_mapping_state
 }
 
 output "lambda_event_source_mapping_state_transition_reason" {
   description = "The reason the event source mapping is in its current state"
-  value       = module.aws_lambda_function_internal_only.lambda_event_source_mapping_state_transition_reason
+  value       = module.aws_lambda_function.lambda_event_source_mapping_state_transition_reason
 }
 
 output "lambda_event_source_mapping_uuid" {
   description = "The UUID of the created event source mapping"
-  value       = module.aws_lambda_function_internal_only.lambda_event_source_mapping_uuid
+  value       = module.aws_lambda_function.lambda_event_source_mapping_uuid
 }
 
 # IAM Role
 output "lambda_role_arn" {
   description = "The ARN of the IAM role created for the Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_role_arn
+  value       = module.aws_lambda_function.lambda_role_arn
 }
 
 output "lambda_role_name" {
   description = "The name of the IAM role created for the Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_role_name
+  value       = module.aws_lambda_function.lambda_role_name
 }
 
 output "lambda_role_unique_id" {
   description = "The unique id of the IAM role created for the Lambda Function"
-  value       = module.aws_lambda_function_internal_only.lambda_role_unique_id
+  value       = module.aws_lambda_function.lambda_role_unique_id
 }
 
 # CloudWatch Log Group
 output "lambda_cloudwatch_log_group_arn" {
   description = "The ARN of the Cloudwatch Log Group"
-  value       = module.aws_lambda_function_internal_only.lambda_cloudwatch_log_group_arn
+  value       = module.aws_lambda_function.lambda_cloudwatch_log_group_arn
 }
 
 output "lambda_cloudwatch_log_group_name" {
   description = "The name of the Cloudwatch Log Group"
-  value       = module.aws_lambda_function_internal_only.lambda_cloudwatch_log_group_name
+  value       = module.aws_lambda_function.lambda_cloudwatch_log_group_name
 }
 
 # Deployment package
 output "local_filename" {
   description = "The filename of zip archive deployed (if deployment was from local)"
-  value       = module.aws_lambda_function_internal_only.local_filename
+  value       = module.aws_lambda_function.local_filename
 
 }
 
 output "s3_object" {
   description = "The map with S3 object data of zip archive deployed (if deployment was from S3)"
-  value       = module.aws_lambda_function_internal_only.s3_object
+  value       = module.aws_lambda_function.s3_object
 }
