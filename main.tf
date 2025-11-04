5104c95bbe74fc4b21d890cb470030edd49d3e25
Lu Wang <lu.wang@alloy.com>
2025-11-04 15:11:50 -0500

    chore: re-run for pre-commit hooks

--- main.tf
+++ main.tf
@@ -143,10 +143,6 @@ resource "aws_lambda_function" "this" {
     var.function_tags
   )
 
-  lifecycle {
-    ignore_changes = [image_uri]
-  }
-
   depends_on = [
     # null_resource.archive,
     # aws_s3_object.lambda_package,
