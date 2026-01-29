be6a2e43a39d335d0f2191483cd2a2695e688aed
Philipp Erbelding <philipp.erbelding@cleverbridge.com>
2026-01-29 09:49:10 +0100

    Add minimal example

---
+++ examples/schedule/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.28"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.0"
+    }
+  }
+}
