e89c4e6e74a36e359bae16a95c4b9ce9280c8f96
Tyler Weiss <tnweiss@users.noreply.github.com>
2026-04-09 15:36:54 -0400

    Fixing aws_region 'region' reference
    AWS Terraform provider version 6.0+ has removed the `region` attribute from the aws_region data source.

--- examples/with-vpc-s3-endpoint/main.tf
+++ examples/with-vpc-s3-endpoint/main.tf
@@ -26,7 +26,7 @@ module "lambda_s3_write" {
 
   environment_variables = {
     BUCKET_NAME = module.s3_bucket.s3_bucket_id
-    REGION_NAME = data.aws_region.current.region
+    REGION_NAME = data.aws_region.current.id
   }
 
   # Let the module create a role for us
@@ -54,7 +54,7 @@ resource "random_pet" "this" {
 }
 
 data "aws_ec2_managed_prefix_list" "this" {
-  name = "com.amazonaws.${data.aws_region.current.region}.s3"
+  name = "com.amazonaws.${data.aws_region.current.id}.s3"
 }
 
 module "vpc" {
@@ -64,7 +64,7 @@ module "vpc" {
   name = random_pet.this.id
   cidr = "10.0.0.0/16"
 
-  azs = ["${data.aws_region.current.region}a", "${data.aws_region.current.region}b", "${data.aws_region.current.region}c"]
+  azs = ["${data.aws_region.current.id}a", "${data.aws_region.current.id}b", "${data.aws_region.current.id}c"]
 
   # Intra subnets are designed to have no Internet access via NAT Gateway.
   intra_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
